d1dae81f54282d3dbdd720dfce145ddb39ae0bd5
Murooj <msms.1424h@gmail.com>
2024-07-19 03:45:50 +0300

    Edit font

--- dist/financialOffersForm.php
+++ dist/financialOffersForm.php
@@ -16,6 +16,10 @@
     <link rel="stylesheet" href="assets/css/app.css">
     <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
 
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         .wizard,
